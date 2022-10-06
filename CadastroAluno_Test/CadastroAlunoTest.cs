2b24e7a80da5f5a310ae2d600e49e4954885051d
GABRIEL GONCALVES DA SILVA <gabriel.gsilva54@senacsp.edu.br>
2022-10-05 22:04:58 -0300

    Finalização de testes nas models e começo de teste na AlunosController

--- CadastroAluno_Test/CadastroAlunoTest.cs
+++ CadastroAluno_Test/CadastroAlunoTest.cs
@@ -43,5 +43,56 @@ namespace CadastroAluno_Test
 
 
         }
+
+        [Theory]
+        [InlineData("Miguel","T94")]
+        [InlineData("Kaue", "T44")]
+        [InlineData(null, "T44")]
+        public void AtualizaDados_ERetornaDadosAtualizadoss(string aluno, string turma)
+        {
+            //Arrange
+            Aluno AlunoTest = new Aluno();
+                AlunoTest.Nome = aluno;
+                AlunoTest.Turma = turma;
+                ;
+            //act
+            _alunotest.AtualizarDados(AlunoTest.Nome, AlunoTest.Turma);
+            //assert
+            Assert.Equal(AlunoTest.Nome, aluno);
+            Assert.Equal(AlunoTest.Turma, turma);
+        }
+
+        [Theory]
+        [InlineData(7)]
+        [InlineData(6)]
+        [InlineData(9)]
+        public void VerificaAprovaçãoERetornaVerdadeiroSeMaiorQ5ERetornaTrue(double media)
+        {
+            //Arrange
+            Aluno AlunoTest = new Aluno();
+            AlunoTest.Media = media;
+            ;
+            //act
+            
+            //assert
+            Assert.True(AlunoTest.VerificaAprovacao());
+        }
+
+        [Theory]
+        [InlineData(7)]
+        [InlineData(6)]
+        [InlineData(9)]
+        public void VerificaAtualizaMediaERetornaValorAtualizado(double media)
+        {
+            //Arrange
+            Aluno AlunoTest = new Aluno();
+            AlunoTest.Media = media;
+            ;
+            //act
+
+            //assert
+            Assert.Equal(AlunoTest.Media,media);
+        }
+
     }
 }
